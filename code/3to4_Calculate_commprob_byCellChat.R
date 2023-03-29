5ff71dcf4298d72913a62e02c86cf17dab6e4437
SBIlab <91528996+SBIlab@users.noreply.github.com>
2023-03-29 23:07:48 +0900

    Add files via upload

---
+++ code/3to4_Calculate_commprob_byCellChat.R
@@ -0,0 +1,87 @@
+setwd("/home/ljh621/cell-cell_communication/github_CCN-immunotherapy/code/CCN_construction")
+library(dplyr)
+library(Matrix)
+source("./CellChat-master/R/CellChat_class.R")
+source("./CellChat-master/R/modeling_modified.R")
+source("./CellChat-master/R/analysis.R")
+source("./CellChat-master/R/utilities.R")
+source("./CellChat-master/R/database.R")
+source("./CellChat-master/R/visualization.R")
+
+## set dataset
+dataset = "Cho"  # VanAllen Liu Gide Hugo Jung Kim Mariathasan PratMelanoma Cho
+
+## set path
+input_path = file.path("../../data/CCN_construction/3_processed_celltype_gene_expression", dataset)
+output_path = file.path("../../data/CCN_construction/4_Commprob_calculated_CellChatObj", dataset)
+if(!file.exists(output_path))
+  dir.create(output_path)
+
+## load cell type GEP file names and define cell types
+deconv_filelist = list.files(path = input_path, recursive = FALSE, pattern = "CIBERSORTxHiRes")
+deconv_filelist = deconv_filelist[ grepl(".txt$", deconv_filelist)]
+cell_type = lapply(strsplit(deconv_filelist, "_"), function(x) { x[3] }) %>% unlist  
+
+message(paste0("input path: ", input_path))
+message(paste0("output path: ", output_path))
+
+## expression data load, define patient name
+cell_type_expList = list()
+for(i in 1:length(deconv_filelist)){ 
+  cell_type_expList[[cell_type[i]]] = read.table(file.path(input_path, deconv_filelist[i]),
+                                                  header = TRUE, sep = "\t", check.names = FALSE)
+}
+patient = colnames(cell_type_expList[[i]])[2:ncol(cell_type_expList[[i]])]
+message(paste0("Number of patients: ", length(patient)))
+
+## CellChat DB load
+load("/home/ljh621/cell-cell_communication/R_code/CellChat-master/data/CellChatDB.human.rda")
+load("/home/ljh621/cell-cell_communication/R_code/CellChat-master/data/PPI.human.rda")
+message("CellChat DB load")
+
+## Calculate communication probabilities of each patient and integrate into comm. pathway
+for(k in 1:length(patient)){
+
+  ## current patient
+  pat = patient[k]
+  message("--------------------------------------------------------------------")
+  message(paste0(pat, ": ", Sys.time()))
+
+  ## patient cell type GEP extraction  
+  pat_exp = data.frame(GeneSymbol = cell_type_expList[[1]]$GeneSymbol)
+  for(j in 1:length(cell_type_expList)){
+    tmp_df = cell_type_expList[[cell_type[j]]][ , match(c("GeneSymbol", pat), colnames(cell_type_expList[[cell_type[j]]]))]
+    colnames(tmp_df)[2] = cell_type[j]
+    pat_exp = left_join(pat_exp, tmp_df, by = "GeneSymbol")
+  }
+  rownames(pat_exp) = pat_exp$GeneSymbol
+  pat_exp = pat_exp[, -1] %>% as.matrix
+ 
+  ## cell type as meta data
+  meta = data.frame(cell_type = cell_type)
+  rownames(meta) = meta$cell_type
+  
+  ## CellChat object creation
+  CellChat = createCellChat(object = pat_exp, meta = meta, group.by = "cell_type")
+  
+  CellChat@DB = CellChatDB.human
+  CellChat = subsetData(CellChat)
+  CellChat@var.features[["features"]] = CellChat@data.signaling@Dimnames[[1]]
+  CellChat = identifyOverExpressedInteractions(CellChat)
+  
+  ## communication probability computation and aggregate
+  CellChat = computeCommunProb(CellChat)
+  if( length(cell_type) <= 4)
+    CellChat = computeCommunProbPathway(CellChat, thresh = 1)
+  else
+    CellChat = computeCommunProbPathway(CellChat)
+  CellChat = aggregateNet(CellChat)
+  
+  ## save CellChat object
+  save(CellChat, file = file.path(output_path, paste0(pat, "_CellChatObj.RData")))
+  message(paste0("save ", pat, " ", Sys.time()))
+  message("--------------------------------------------------------------------")
+}
+
+
+
---
+++ code/4to5_CCN_extract_from_CellChatObj.R
@@ -0,0 +1,73 @@
+setwd("/home/ljh621/cell-cell_communication/github_CCN-immunotherapy/code/CCN_construction")
+library(dplyr)
+library(reticulate)
+source_python("../ML/function/DataLoader.py")
+
+
+## dataset and output path setting
+dataset = "VanAllen"
+input_path = file.path("../../data/CCN_construction/4_Commprob_calculated_CellChatObj", dataset)
+output_path = file.path("../../data/CCN_construction/5_CCN", dataset)
+if(!file.exists(output_path))
+    dir.create(output_path)
+
+message(dataset)
+
+
+## dataset and response info
+clinical = nonres_res_partition(dataset)
+nonresponder_sample = filter(clinical, response == 0) %>% rownames
+responder_sample = filter(clinical, response == 1) %>% rownames
+
+## CellChat object filelist
+obj_filelist = list.files(input_path, recursive = FALSE, pattern = ".RData" )
+obj_filelist = obj_filelist[ match(rownames(clinical), lapply(strsplit(obj_filelist, "_CellChatObj"), function(x) { x[1] })) ]
+
+
+## CCN_matrix by pathway
+load(file.path(input_path, obj_filelist[1]))
+pathway = CellChat@netP$pathways
+for(j in 2:length(obj_filelist)){
+  load(file.path(input_path, obj_filelist[j]))
+  pathway = intersect(pathway, CellChat@netP$pathways)
+}
+pathway = sort(pathway)
+
+
+PairedMatrix = list()
+for(p in 1:length(pathway)){
+    
+    for(i in 1:length(obj_filelist)){
+        load(file.path(input_path, obj_filelist[i]))
+        pat = strsplit(obj_filelist[i], "_CellChatObj")[[1]][1]
+        PairedMatrix[[pathway[p]]][[pat]] = list()
+
+        commprob_pathway = CellChat@netP$prob[, , pathway[p]]
+        for(m in 1:dim(commprob_pathway)[1]){
+            for(n in 1:dim(commprob_pathway)[2]){
+                source = rownames(commprob_pathway)[m]
+                target = colnames(commprob_pathway)[n]
+
+                PairedMatrix[[pathway[p]]][[pat]][[paste0(source, "_to_", target)]] = commprob_pathway[source, target]
+            }
+        }
+        PairedMatrix[[pathway[p]]][[pat]] = do.call(cbind, PairedMatrix[[pathway[p]]][[pat]]) %>% as.data.frame
+    }
+    PairedMatrix[[pathway[p]]] = do.call(rbind, PairedMatrix[[pathway[p]]]) %>% as.data.frame
+    message(p, " ", pathway[p])
+
+}
+CCN_matrix = do.call(cbind, PairedMatrix) %>% as.data.frame
+summed_CCN = apply(CCN_matrix, 2, sum)
+col = summed_CCN[summed_CCN != 0] %>% names
+CCN_matrix = CCN_matrix[, col]
+
+
+## aggregated and file out
+nonresponder_CCN = cbind( sample_id = nonresponder_sample, CCN_matrix[ nonresponder_sample ,] )
+responder_CCN = cbind( sample_id = responder_sample, CCN_matrix[ responder_sample ,])
+write.table(nonresponder_CCN, file.path(output_path, "nonresponder_CCN.txt"),
+            col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
+write.table(responder_CCN, file.path(output_path, "responder_CCN.txt"),
+            col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
+
