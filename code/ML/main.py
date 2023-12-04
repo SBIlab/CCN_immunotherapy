990fd51cff449653bb28a92510dcbdba92fe58c2
SBIlab <91528996+SBIlab@users.noreply.github.com>
2023-12-04 22:08:24 +0900

    Update main.py

--- code/ML/main.py
+++ code/ML/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import os
-import itertools
 exec(open("./function/DataLoader.py").read())
 # exec(open("./function/Control_DataLoader.py").read())
 exec(open("./function/ML.py").read())
